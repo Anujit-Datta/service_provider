e2c714cb27e791680604fa5c443531aa9bcfdc00
Anujit-Datta <anujitbestbd@gmail.com>
2023-10-31 01:22:56 +0600

    refactored register page

--- lib/Pages/Register.dart
+++ lib/Pages/Register.dart
@@ -23,13 +23,7 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
         body: Stack(
           children: [
-            Container(
-              padding: EdgeInsets.only(left: 35, top: 30),
-              child: Text(
-                'Create\nAccount',
-                style: TextStyle(color: Colors.white, fontSize: 33),
-              ),
-            ),
+            createAccountText(),
             SingleChildScrollView(
               child: Container(
                 padding: EdgeInsets.only(
@@ -41,123 +35,23 @@ class _RegisterPageState extends State<RegisterPage> {
                       margin: EdgeInsets.only(left: 35, right: 35),
                       child: Column(
                         children: [
-                          TextField(
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Colors.black,
-                                  ),
-                                ),
-                                hintText: "Name",
-                                hintStyle: TextStyle(color: Colors.white),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
-                          ),
+                          nameField(),
                           SizedBox(
                             height: 30,
                           ),
-                          TextField(
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Colors.black,
-                                  ),
-                                ),
-                                hintText: "Email",
-                                hintStyle: TextStyle(color: Colors.white),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
-                          ),
+                          emailField(),
                           SizedBox(
                             height: 30,
                           ),
-                          TextField(
-                            style: TextStyle(color: Colors.white),
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Colors.black,
-                                  ),
-                                ),
-                                hintText: "Password",
-                                hintStyle: TextStyle(color: Colors.white),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
-                          ),
+                          passwordField(),
                           SizedBox(
                             height: 40,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Sign Up',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 27,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                              CircleAvatar(
-                                radius: 30,
-                                backgroundColor: Color(0xff4c505b),
-                                child: IconButton(
-                                    color: Colors.white,
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.arrow_forward,
-                                    )),
-                              )
-                            ],
-                          ),
+                          signupButton(),
                           SizedBox(
                             height: 40,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pushReplacementNamed(context, '/login');
-                                },
-                                child: Text(
-                                  'Sign In',
-                                  textAlign: TextAlign.left,
-                                  style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      color: Colors.white,
-                                      fontSize: 18),
-                                ),
-                                style: ButtonStyle(),
-                              ),
-                            ],
-                          )
+                          signinAsk(context)
                         ],
                       ),
                     )
@@ -167,6 +61,137 @@ class _RegisterPageState extends State<RegisterPage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Row signinAsk(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          'Already have an account? ',
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/login');
+          },
+          child: Text(
+            'Sign In',
+            style: TextStyle(
+                decoration: TextDecoration.underline,
+                color: Colors.white,
+                fontSize: 20),
+          ),
+          style: ButtonStyle(),
+        ),
+      ],
+    );
+  }
+
+  Row signupButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Sign Up',
+          style: TextStyle(
+              color: Colors.white, fontSize: 27, fontWeight: FontWeight.w700),
+        ),
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: Color(0xff4c505b),
+          child: IconButton(
+              color: Colors.white,
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_forward,
+              )),
+        )
+      ],
+    );
+  }
+
+  TextField passwordField() {
+    return TextField(
+      style: TextStyle(color: Colors.white),
+      obscureText: true,
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.black,
+            ),
+          ),
+          hintText: "Password",
+          hintStyle: TextStyle(color: Colors.white),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+  }
+
+  TextField emailField() {
+    return TextField(
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.black,
+            ),
+          ),
+          hintText: "Email",
+          hintStyle: TextStyle(color: Colors.white),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+  }
+
+  TextField nameField() {
+    return TextField(
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: Colors.black,
+            ),
+          ),
+          hintText: "Name",
+          hintStyle: TextStyle(color: Colors.white),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+  }
+
+  Container createAccountText() {
+    return Container(
+      padding: EdgeInsets.only(left: 35, top: 30),
+      child: Text(
+        'Create\nAccount',
+        style: TextStyle(color: Colors.white, fontSize: 45),
       ),
     );
   }
